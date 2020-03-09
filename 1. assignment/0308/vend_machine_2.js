499f96261b1e2cdc915aacfb19a9e51533cd2b56
chaameliaa <chaemilee9012@gmail.com>
2020-03-09 21:15:36 +0900

    0309

--- 1. assignment/0308/vend_machine_2.js
+++ 1. assignment/0308/vend_machine_2.js
@@ -1,1 +1,12 @@
-var 
+var articleJs = document.querySelector('article');
+
+articleJs.onclick = function(){
+    var target = event.target;
+    // console.log(target);
+
+    if(target.type == 'button'){
+        // console.log('ok');
+        var pJs = articleJs.querySelectorAll('p');
+    }
+
+}
