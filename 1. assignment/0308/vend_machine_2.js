37d00723924be5ed6847819f5cbbf14b4a2f8e3b
chaemilee9012 <chaemilee9012@gmail.com>
2020-03-10 11:01:19 +0900

    0310

--- 1. assignment/0308/vend_machine_2.js
+++ 1. assignment/0308/vend_machine_2.js
@@ -1,12 +1,13 @@
 var articleJs = document.querySelector('article');
+var pJs = document.querySelectorAll('p');
 
 articleJs.onclick = function(){
     var target = event.target;
-    // console.log(target);
 
-    if(target.type == 'button'){
-        // console.log('ok');
-        var pJs = articleJs.querySelectorAll('p');
+    for(var i = 0; i < pJs.length; i++){
+        pJs[i].style.display = 'none';
+        if(target.className == pJs[i].className){
+            pJs[i].style.display = 'block'
+        }
     }
-
 }
