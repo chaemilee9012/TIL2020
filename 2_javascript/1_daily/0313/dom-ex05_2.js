6c7205c2782637f4e3a4120c7566db0d36e7cddc
chaemilee9012 <chaemilee9012@gmail.com>
2020-03-20 18:07:37 +0900

    0320

--- 2_javascript/1_daily/0313/dom-ex05_2.js
+++ 2_javascript/1_daily/0313/dom-ex05_2.js
@@ -1,88 +1,201 @@
 window.addEventListener('DOMContentLoaded', function() {
 /*
-1_1. 이미지를 클릭한다.
-1_2. 클릭한 이미지에 active 클래스 더한다.
-1_3. 클릭한 이미지의 src 값을 구한다.
-1_4. popup에 appear 클래스 더한다.
-
-2_1. popup 클릭한다.
-2_2. popup에 appear 클래스 뺀다.
+1. img를 클릭한다.
+2. 클릭한 img에 active 클래스 추가한다.
+3. popup img src를 클릭한 img src 값으로 바꾼다.
+4. popup에 appear 클래스 추가한다.
 */
-  var thumDiv = document.querySelector('.thum');
-  var thumImg = thumDiv.querySelectorAll('img');
-  var popUp = document.querySelector('.popup');
-  var popImg = popUp.querySelector('img');
-  var idx;
 
-  thumDiv.addEventListener('click', function(e) {
-    // console.log(e.target.src);
-    for(var i = 0; i < thumImg.length; i++) {
-      thumImg[i].classList.remove('active');
+  var divThum = document.querySelector('.thum');
+  var imgThum = document.querySelectorAll('.thum img');
+  var imgPop = document.querySelector('.popup img');
+  var divPop = document.querySelector('.popup');
+  var prevBtn = document.querySelector('.prev');
+  var nextBtn = document.querySelector('.next');
+  var dNum;
+  
+  divThum.addEventListener('click', function(e) {
+    for(var i = 0; i < imgThum.length; i++) {
+      imgThum[i].classList.remove('active');
     }
-    idx = e.target.dataset.num;
     e.target.classList.add('active');
-    popImg.src = e.target.src;
-    popUp.classList.add('appear');
-    disappear();
+    imgPop.src = e.target.src;
+    divPop.classList.add('appear');
+    dNum = e.target.dataset.num;
   });
 
-  popUp.addEventListener('click', function(e) {
+  divPop.addEventListener('click', function(e) {
+    if(e.target.nodeName == 'A') {
+      return;
+    }
     this.classList.remove('appear');
   });
 
 /*
-1. a태그 클릭한다.
-2. prev → 
-   next → 
-3. 첫번째 img에서 prev 숨긴다.
-   마지막 img에서 next 숨긴다.
+1. a태그를 클릭한다.
+2. 화면에 보이는 img의 data num을 가져온다.
+3. prev: data num--
+   next: data num++
+4. data num == 0: prev 숨기기
+   data num == 2: next 숨기기
 */
-  var prevBtn = document.querySelector('.prev');
-  var nextBtn = document.querySelector('.next');
-
   prevBtn.addEventListener('click', function() {
-    --idx;
-    change();
-    disappear();
+    dNum--;
+    imgChange();
   });
 
   nextBtn.addEventListener('click', function() {
-    ++idx;
-    change();
-    disappear();
+    dNum++;
+    imgChange();
   });
 
-  function change() {
-    popImg.src = thumImg[idx].src;
+  function imgChange() {
+    imgPop.src = imgThum[dNum].src;
   }
 
   function disappear() {
-    if(idx == 0) {
+    if(dNum == 0) {
       prevBtn.style.opacity = 0;
-    } else if(idx == 2) {
+    } else if(dNum == 2) {
       nextBtn.style.opacity = 0;
     } else {
       prevBtn.style.opacity = 1;
       nextBtn.style.opacity = 1;
+      
     }
   }
 
-
-  // var btn = popUp.querySelector('.button');
-
-  // btn.addEventListener('click', function(e) {
-  //   console.log(idx);
-  //   if (e.target.textContent == 'prev'){
-  //     console.log('prev');
-  //     popImg.src = thumImg[--idx].src;
-  //   } else {
-  //     console.log('next');
-  //     popImg.src = thumImg[++idx].src;
-  //   }
-    // thumImg[++idx]
-    // var prev = btn.children[0];
-    // var next = btn.children[1];
-  // });
-
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener('DOMContentLoaded', function() {
+// /*
+// 1_1. 이미지를 클릭한다.
+// 1_2. 클릭한 이미지에 active 클래스 더한다.
+// 1_3. 클릭한 이미지의 src 값을 구한다.
+// 1_4. popup에 appear 클래스 더한다.
+
+// 2_1. popup 클릭한다.
+// 2_2. popup에 appear 클래스 뺀다.
+// */
+//   var thumDiv = document.querySelector('.thum');
+//   var thumImg = thumDiv.querySelectorAll('img');
+//   var popUp = document.querySelector('.popup');
+//   var popImg = popUp.querySelector('img');
+//   var idx;
+
+//   thumDiv.addEventListener('click', function(e) {
+//     // console.log(e.target.src);
+//     for(var i = 0; i < thumImg.length; i++) {
+//       thumImg[i].classList.remove('active');
+//     }
+//     idx = e.target.dataset.num;
+//     e.target.classList.add('active');
+//     popImg.src = e.target.src;
+//     popUp.classList.add('appear');
+//     disappear();
+//   });
+
+//   popUp.addEventListener('click', function(e) {
+//     this.classList.remove('appear');
+//   });
+
+// /*
+// 1. a태그 클릭한다.
+// 2. prev → 
+//    next → 
+// 3. 첫번째 img에서 prev 숨긴다.
+//    마지막 img에서 next 숨긴다.
+// */
+//   var prevBtn = document.querySelector('.prev');
+//   var nextBtn = document.querySelector('.next');
+
+//   prevBtn.addEventListener('click', function() {
+//     --idx;
+//     change();
+//     disappear();
+//   });
+
+//   nextBtn.addEventListener('click', function() {
+//     ++idx;
+//     change();
+//     disappear();
+//   });
+
+//   function change() {
+//     popImg.src = thumImg[idx].src;
+//   }
+
+//   function disappear() {
+//     if(idx == 0) {
+//       prevBtn.style.opacity = 0;
+//     } else if(idx == 2) {
+//       nextBtn.style.opacity = 0;
+//     } else {
+//       prevBtn.style.opacity = 1;
+//       nextBtn.style.opacity = 1;
+//     }
+//   }
+
+
+//   // var btn = popUp.querySelector('.button');
+
+//   // btn.addEventListener('click', function(e) {
+//   //   console.log(idx);
+//   //   if (e.target.textContent == 'prev'){
+//   //     console.log('prev');
+//   //     popImg.src = thumImg[--idx].src;
+//   //   } else {
+//   //     console.log('next');
+//   //     popImg.src = thumImg[++idx].src;
+//   //   }
+//     // thumImg[++idx]
+//     // var prev = btn.children[0];
+//     // var next = btn.children[1];
+//   // });
+
+
+// });
---
+++ 2_javascript/1_daily/0320/js/index.js
@@ -0,0 +1,61 @@
+window.addEventListener('DOMContentLoaded', function() {
+/*
+1. img를 클릭한다.
+2. ly-pop img src를 클릭한 img src로 바꾼다.
+3. ly-pop에 active 클래스를 추가한다.
+4. ly-pop를 클릭하면 active 클래스 뺀다.
+*/
+  var ulGal = document.querySelector('.gallery');
+  var imgGal = ulGal.querySelectorAll('img');
+  var figcGal = ulGal.querySelectorAll('figcaption');
+  var lyPop = document.querySelector('.ly-pop');
+  var imgPop = lyPop.querySelector('img');
+  var divPop = lyPop.querySelector('div');
+  var figcPop = lyPop.querySelector('figcaption');
+  var dNum;
+
+  ulGal.addEventListener('click', function(e) {
+    if(e.target.nodeName != 'IMG') {
+      return;
+    }
+    dNum = e.target.dataset.num;
+    changeContents();
+    lyPop.classList.add('active');
+  });
+  
+  lyPop.addEventListener('click', function(e) {
+    if(e.target.nodeName == 'A') {
+      return;
+    }
+    this.classList.remove('active');
+  });
+/*
+1. a태그 클릭한다.
+2. 이전에 클릭한 img의 dNum을 가져온다
+3. prev: dNum--
+   next: dNum++
+4. dNum == 0: prev 숨긴다.
+   dNum == 5: next 숨긴다.
+*/
+  divPop.addEventListener('click', function(e) {
+    if(e.target.className == 'prev') {
+      dNum--;
+    } else {
+      dNum++;
+    }
+    changeContents();
+  });
+
+  function changeContents() {
+    imgPop.src = imgGal[dNum].src;
+    figcPop.textContent = figcGal[dNum].textContent;
+    for(var i = 0; i < divPop.children.length; i++) {
+      divPop.children[i].style.display = 'inline-block';
+    }
+    if(dNum == 0) {
+      divPop.children[0].style.display = 'none';
+    } else if(dNum == 5) {
+      divPop.children[1].style.display = 'none';
+    }
+  }
+});
