534937bddd0412ab6f72164f5d9dd58159f65371
chaemilee9012 <chaemilee9012@gmail.com>
2020-03-31 16:43:58 +0900

    0331

--- 2_javascript/1_daily/0330/ajax_worklist/common.js
+++ 2_javascript/1_daily/0330/ajax_worklist/common.js
@@ -12,6 +12,7 @@ $(function(){
       */
       var thum, name, time;
       var workMain, workList='';
+      var url;
       
       $(data).find('work').each(function(i) {
         thum = $(this).find('thum').text();
@@ -20,8 +21,10 @@ $(function(){
 
         if(!$('section').hasClass('work')){
           funcMain(i);
+          $('section').append(workMain);
         } else {
           funcSub(i);
+          $('section ul').append(workList);
         }
       });
 
@@ -34,7 +37,6 @@ $(function(){
                   --></figure>`;
         // sub.html#idx: 클릭한 a태그 구분용
          
-        $('section').append(workMain);
       }
 
       function funcSub() {
@@ -58,18 +60,36 @@ $(function(){
         workList += '</figure>';
         workList += '</li>';
         // console.log(workList);
-
-        $('section ul').append(workList);
-
-        $('section ul li').on('click', () => {
-          console.log('a');
-        });
       }
-      
-      var url = location.hash.substr(1);
+
+      url = location.hash.substr(1);
       // location.hash: 주소값에서 #값 접근
       // console.log(url);
       $('section ul li').eq(url).addClass('active');
+
+      $('section ul li').on('click', function() {
+        /*
+        방법 1) data 값으로 처리
+        방법 2) each 안에 변수로 처리
+        */
+        // 방법 1) each 안에 data 값으로 처리
+        var subIdx = $(this).index();
+        showDetail(subIdx);
+      });
+
+      function showDetail(detIdx) {
+        var dataDet = $(data).find('work').eq(detIdx).find('detail').text();
+        var dataName = $(data).find('work').eq(detIdx).find('name').text();
+        var dataTime = $(data).find('work').eq(detIdx).find('time').text();
+        var dataLink = $(data).find('work').eq(detIdx).find('link').text();
+
+        $('article img').attr('src', dataDet);
+        $('article figcaption span').eq(0).text(dataName);
+        $('article figcaption span').eq(1).text(dataTime);
+        $('article figcaption a').attr('href', dataLink); 
+      }
+
+      showDetail(url);
     }
   });
   //end
