b4ff74d8e7b2ce2777faa478179e3f6152af8c81
chaemilee9012 <chaemilee9012@gmail.com>
2020-03-31 18:19:21 +0900

    0331

---
+++ 2_javascript/1_daily/0331/common.js
@@ -0,0 +1,35 @@
+$(function(){
+  //start
+  $.ajax({ // ajax: jq 외부 파일 호출 메소드
+    url: 'data.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data){
+      var sectionJs = document.querySelector('section');
+      var figureJs;
+      var thumSrc, nameTxt;
+
+      for(let i in data.portfolio) { // 2번
+        thumSrc = data.portfolio[i].work[0].thum;
+        nameTxt = data.portfolio[i].work[2].name;
+        linkSub = data.portfolio[i].work[4].link;
+
+        funcMain(i);
+        
+        for(var j in data.portfolio[i].work) { // 5번
+        }
+      }
+
+      function funcMain(idx) {
+        sectionJs.innerHTML += `<figure>
+                                  <a href="#">
+                                    <img src="${thumSrc}" alt="">
+                                    <figcaption>${nameTxt}</figcaption>
+                                  </a>
+                                </figure>`;
+        figureJs = document.querySelectorAll('figure');
+      }
+    }
+  });
+  //end
+});
