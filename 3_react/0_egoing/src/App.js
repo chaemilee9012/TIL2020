b721f84fbb9432430f892613cfe2aa1e63998fce
chaemilee9012 <chaemilee9012@gmail.com>
2020-06-18 01:37:11 +0900

    0617

--- 3_react/0_egoing/src/App.js
+++ 3_react/0_egoing/src/App.js
@@ -4,56 +4,57 @@ import Navigation from "./components/Navigation";
 import Contents from "./components/Contents";
 import "./App.css";
 
-// class Subject extends Component {
-//   render() {
-//     return (
-//       <header>
-//         {/* <h1>WEB</h1>
-//         world wide web! */}
-//         <h1>{this.props.title}</h1>
-//         {this.props.sub}
-//       </header>
-//     );
-//   }
-// }
-
-// class Navigation extends Component {
-//   render() {
-//     return(
-//       <nav>
-//         <ul>
-//           <li><a href="1.html">HTML</a></li>
-//           <li><a href="2.html">CSS</a></li>
-//           <li><a href="3.html">JAVASCRIPT</a></li>
-//         </ul>
-//       </nav>
-//     );
-//   }
-// }
-
-// class Contents extends Component {
-//   render() {
-//     return (
-//       <article>
-//         {/* <h2>HTML</h2>
-//         HTML is HyperText Markup Language. */}
-//         <h2>{this.props.title}</h2>
-//         {this.props.dscrt}
-//       </article>
-//     );
-//   }
-// }
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'welcome',
+      selectedId: 2,
+      welcome: {title: 'Welcome', dscrt: 'Hello, React!'},
+      subject: {title: 'WEB', sub: 'world wide web!'},
+      contents: [
+        {id: 1, title: 'HTML', dscrt: 'HTML is HyperText...'},
+        {id: 2, title: 'CSS', dscrt: 'CSS is Cascading...'},
+        {id: 3, title: 'Javascript', dscrt: 'Javascript is...'}
+      ],
+    }
+  }
+  
   render() {
+    var _title, _dscrt = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _dscrt = this.state.welcome.dscrt;
+    } else if(this.state.mode === 'read') {
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if(data.id === this.state.selectedId) {
+          _title = data.title;
+          _dscrt = data.dscrt;
+          break;
+        }
+        i = i + 1;
+      }
+    }
     return (
-      // <div className="App">Hello, React!</div>
       <div className="App">
-        {/* <Subject></Subject> */}
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        {/* <Subject title="React" sub="For UI"></Subject> */}
-        <Navigation></Navigation>
-        <Contents title="HTML" dscrt="HTML is HyperText Markup Language."></Contents>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function() {
+            this.setState({mode: 'welcome'});
+          }.bind(this)}
+        ></Subject>
+        <Navigation
+          data={this.state.contents}
+          onChangePage={function(id) {
+            this.setState({
+              mode: 'read',
+              selectedId: parseInt(id)});
+          }.bind(this)}
+        ></Navigation>
+        <Contents title={_title} dscrt={_dscrt}></Contents>
       </div>
     );
   }
