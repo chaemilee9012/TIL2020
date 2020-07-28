490518c29cd61fc0d5f885875ee8d70635a71486
chaemilee9012 <chaemilee9012@gmail.com>
2020-07-28 17:43:01 +0900

    리액트를 다루는 기술

--- 3_react/src/App.js
+++ 3_react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './2_velopert/todo_app/TodoTemplate';
 import TodoInsert from './2_velopert/todo_app/TodoInsert';
 import TodoList from './2_velopert/todo_app/TodoList';
@@ -44,25 +44,43 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT': // 새로 추가
+      // { type: 'INSERT', todo: { id: 1, text: 'todo', checked: false } }
+      return todos.concat(action.todo);
+    case 'REMOVE': // 제거
+      // { type: 'REMOVE', id: 1 }
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE': // 토글
+      // { type: 'TOGGLE', id: 1 }
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,);
+    default: 
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState(
-    createBulkTodos
-  // [{
-  //     id: 1,
-  //     text: '리액트 기초',
-  //     checked: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     text: '컴포넌트 스타일링',
-  //     checked: true,
-  //   },
-  //   {
-  //     id: 3,
-  //     text: '기능 구현',
-  //     checked: false,
-  //   },]
-  );
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  // const [todos, setTodos] = useState(
+  //   createBulkTodos
+  // // [{
+  // //     id: 1,
+  // //     text: '리액트 기초',
+  // //     checked: true,
+  // //   },
+  // //   {
+  // //     id: 2,
+  // //     text: '컴포넌트 스타일링',
+  // //     checked: true,
+  // //   },
+  // //   {
+  // //     id: 3,
+  // //     text: '기능 구현',
+  // //     checked: false,
+  // //   },]
+  // );
 
   // 고유값으로 사용될 id
   // ref 사용하여 변수 담기
@@ -75,7 +93,8 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos => todos.concat(todo));
+      // setTodos(todos => todos.concat(todo)); // useState
+      dispatch({ type: 'INSERT', todo }); // useReducer
       nextId.current += 1;
     },
     [],
@@ -83,18 +102,20 @@ const App = () => {
 
   const onRemove = useCallback(
     id => {
-      setTodos(todos => todos.filter(todo => todo.id !== id));
+      // setTodos(todos => todos.filter(todo => todo.id !== id)); // useState
+      dispatch({ type: 'REMOVE', id }); // useReducer
     },
     [],
   );
 
   const onToggle = useCallback(
     id => {
-      setTodos( todos =>
-        todos.map(todo => 
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
+      // setTodos( todos =>
+      //   todos.map(todo => 
+      //     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      //   ),
+      // ); // useState
+      dispatch({ type: 'TOGGLE', id }); // useReducer
     },
     [],
   );
