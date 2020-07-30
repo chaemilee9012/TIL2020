0e375d3772530f640e5a46887b6ad805137482e5
chaemilee9012 <chaemilee9012@gmail.com>
2020-07-30 20:47:12 +0900

    리액트를 다루는 기술

--- 3_react/src/2_velopert/router_tutorial/About.js
+++ 3_react/src/2_velopert/router_tutorial/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true // 문자열 맨 앞 ? 생략
+  });
+
+  const showDetail = query.detail === 'true'; // 쿼리의 파싱 결과 값은 문자열
   return (
     <div>
       <h1>소개</h1>
       <p>리액트 라우터 기초를 실습해보는 예제 프로젝트</p>
+      {showDetail && <p>detail 값: true</p>}
     </div>
   );
 };
